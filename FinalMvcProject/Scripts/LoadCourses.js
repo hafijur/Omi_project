fd1774b3964a09f51974f9e708be237e9390c6a4
amigitgai <hafijur30000@gmail.com>
2016-03-07 08:10:44 +0600

    all deleted

--- FinalMvcProject/Scripts/LoadCourses.js
+++
@@ -1,7 +0,0 @@
-﻿$(function () {
-    $("#DepartmentId").change(function () {
-        var dept = $("#DepartmentId").val();
-        $("#Departments").load('@Url.Action("LoadCourses")', { id: dept });
-
-    });
-});
